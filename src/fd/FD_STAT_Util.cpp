5aa1044baf883ff665728d7003bdfa9263aae503
Lucio-Kaup-th-ab-de <115980381+Lucio-Kaup-th-ab-de@users.noreply.github.com>
2023-04-16 11:26:57 +0200

    Update FD_STAT_Util.cpp

--- src/fd/FD_STAT_Util.cpp
+++ src/fd/FD_STAT_Util.cpp
@@ -1,5 +1,7 @@
 #include "FD_STAT_Util.h"
 #include <boost/math/distributions/students_t.hpp>
+
+//Berechnet die Wahrscheinlichkeit eines bestimmten Wertes
 double FD_STAT_Util::students_t_pdf(double x, int df){
     double degrees_of_freedom = static_cast<double>(df);    
     boost::math::students_t dist_stud_t{degrees_of_freedom};
@@ -7,6 +9,7 @@ double FD_STAT_Util::students_t_pdf(double x, int df){
     return result;
 }
 
+// Berechnet die Kumulative Wahrscheinlichkeit bis zu einem Punkt (=> Integral der Fläche)
 double FD_STAT_Util::students_t_cdf(double x, int df){
     double degrees_of_freedom = static_cast<double>(df);
     boost::math::students_t dist_stud_t{degrees_of_freedom};
