e21c875af29527f75f05b2070dafd7a1d4f21439
Lucio-Kaup-th-ab-de <115980381+Lucio-Kaup-th-ab-de@users.noreply.github.com>
2023-05-23 12:49:00 +0200

    Update FD_STAT_Util_test.cpp
    Quantiltests hinzugefügt

--- src/test/FD_STAT_Util_test.cpp
+++ src/test/FD_STAT_Util_test.cpp
@@ -1,27 +1,36 @@
 #include "doctest.h"
 #include "FD_STAT_Util.h"
 
-
 TEST_SUITE_BEGIN("FD_STAT_Util");
-TEST_CASE("get pdf value for 1.976, df = 2") 
+TEST_CASE("get pdf value for 1.976, df = 2")
 {
     FD_STAT_Util stat_util{};
     CHECK(stat_util.students_t_pdf(1.976, 2) == doctest::Approx(0.0696975));
 }
-TEST_CASE("get pdf value for 1.976, df = 8") 
+TEST_CASE("get pdf value for 1.976, df = 8")
 {
     FD_STAT_Util stat_util{};
     CHECK(stat_util.students_t_pdf(1.976, 8) == doctest::Approx(0.0646495));
 }
-TEST_CASE("get cdf value for 0.05, df = 7") 
+TEST_CASE("get cdf value for 0.05, df = 7")
 {
     FD_STAT_Util stat_util{};
     CHECK(stat_util.students_t_cdf(0.05, 7) == doctest::Approx(0.5192404));
 }
-TEST_CASE("get cdf value for 0.95, df = 5") 
+TEST_CASE("get cdf value for 0.95, df = 5")
 {
     FD_STAT_Util stat_util{};
     CHECK(stat_util.students_t_cdf(0.95, 5) == doctest::Approx(0.8071313));
+}
+TEST_CASE("get quantile value for 0.01, df = 6")
+{
+    FD_STAT_Util stat_util{};
+    CHECK(stat_util.students_t_quantile(0.01, 6) == doctest::Approx(-3.14267));
+}
+TEST_CASE("get quantile value for 0.05, df = 8")
+{
+    FD_STAT_Util stat_util{};
+    CHECK(stat_util.students_t_cdf(0.05, 8) == doctest::Approx(0.519326));
 }
 
 TEST_SUITE_END();
