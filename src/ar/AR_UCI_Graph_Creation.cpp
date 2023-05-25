95abc1633f3ba293ace68d02383ba0a30d951fd4
Lucio-Kaup-th-ab-de <115980381+Lucio-Kaup-th-ab-de@users.noreply.github.com>
2023-05-25 09:57:15 +0200

    Beta und Power Berechnung korrigiert
    Bei bestimmten Effekt Einstellungen waren die berechneten Output Werte fehlerhaft
    Die neue Berechnung verwendet Rechtecke um die die Fläche unter dem Graphen annähernd zu berechnen

--- src/ar/AR_UCI_Graph_Creation.cpp
+++ src/ar/AR_UCI_Graph_Creation.cpp
@@ -20,20 +20,26 @@ void AR_UCI_Graph_Creation::create_graph(int df, double alpha, double input_effe
   a_border = stat_util.students_t_quantile(alpha / 2, df);
   c_border = stat_util.students_t_quantile(1 - alpha / 2, df);
 
-  // Runde a_border und c_border auf 0,1, damit die Flächen besser an die Grenzen anschließen
+  // Runde a_border und c_border auf 0.1, damit die Flächen besser an die Grenzen anschließen
   a_border = std::round(a_border * 10) / 10.0;
   c_border = std::round(c_border * 10) / 10.0;
 
   // Power- und Beta-Berechnung
-  double t_critical = stat_util.students_t_quantile(1 - alpha, df);
-  double t_shifted = (t_critical * sqrt(df) - (input_effect / 80) * sqrt(df + 1)) / sqrt(df);
-  power = 1 - stat_util.students_t_cdf(t_shifted, df);
-  beta = 1 - power;
+  const double interval = 0.1;
+  const double x_start = -10.0;
+  double x_end = c_border - input_effect/80;
+  double beta = 0.0;
+
+  for (double x = x_start; x <= x_end; x += interval)
+  {
+    double y = stat_util.students_t_pdf(x, df);
+    beta += y * interval; // Rechteecke Approximieren die Fläche unter dem Graph
+  }
 
   // Runden auf 5 Nachkommastellen
   double factor = std::pow(10, 5); // Die 5 legt die Nachkommastellen fest
-  power = std::round(power * factor) / factor;
   beta = std::round(beta * factor) / factor;
+  power = 1 - beta;
 
   graph_presenter.present_graph(point_list, a_border, c_border, input_effect, power, beta, antialias);
   graph_presenter.present_outputs(power, beta);
